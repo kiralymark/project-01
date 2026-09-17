9bb300825e36949b419a0e78bd073425a0615943
Mark Kiraly <kmark2062@gmail.com>
2026-09-17 19:44:05 +0200

    Modify scripts/character_body_2d.gd

--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -1,25 +1,24 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+const SPEED:float = 300.0
+#var _direction_map:Dictionary[Key,Vector2] = {KEY_W: Vector2(0,-1), KEY_S: Vector2(0,1), KEY_A: Vector2(-1,0), KEY_D: Vector2(1,0)}
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	var direction:Vector2 = Vector2(0,0)
+	if Input.is_action_pressed("ui_up"):		# is InputEventKey:
+		#if _direction_map.has(InputEventKey.keycode) && InputEventKey.pressed:
+		#	var direction:Vector2 = _direction_map[InputEventKey.keycode]
+		#	#velocity.x = direction * SPEED
+		direction = Vector2(0,-1)
+	if Input.is_action_pressed("ui_down"):
+		direction = Vector2(0,1)
+	if Input.is_action_pressed("ui_left"):
+		direction = Vector2(-1,0)
+	if Input.is_action_pressed("ui_right"):
+		direction = Vector2(1,0)
+	
+	velocity = direction * SPEED
+	
 	move_and_slide()
