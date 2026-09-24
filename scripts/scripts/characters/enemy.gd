a35d40dc31160314dfe76c4a4f21e1c141e30aad
IndianJanos <bekessy.leo@gmail.com>
2026-09-24 16:14:24 +0200

    Camera and enemy
    Camera movement and enemy behaviour

--- scripts/scripts/characters/enemy.gd
+++ scripts/scripts/characters/enemy.gd
@@ -9,6 +9,11 @@ func _ready() -> void:
 
 func _physics_process(_delta: float) -> void:
 	var player_position: Vector2 = _player.global_position
+	var distance_squared: float = global_position.distance_squared_to(player_position)
+	
+	if distance_squared > 40000.0:
+		return
+	
 	var direction = (player_position - global_position).normalized()
 	
 	velocity = direction * SPEED
